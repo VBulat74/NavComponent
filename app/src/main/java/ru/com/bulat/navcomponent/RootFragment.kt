6fa6a548472e5ba371edd1b3895f38c00d40e2c4
VBulat74 <Vladimir.Bulat@gmail.com>
2023-07-18 10:37:53 +0300

    Leson 02 save arguments

--- app/src/main/java/ru/com/bulat/navcomponent/RootFragment.kt
+++ app/src/main/java/ru/com/bulat/navcomponent/RootFragment.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import ru.com.bulat.navcomponent.databinding.FragmentRootBinding
 
 class RootFragment : Fragment(R.layout.fragment_root) {
@@ -26,16 +27,39 @@ class RootFragment : Fragment(R.layout.fragment_root) {
         binding.openYellowBoxButton.setOnClickListener {
             openBox(Color.rgb(255,255,200), "Yellow")
         }
-        parentFragmentManager.setFragmentResultListener(BoxFragment.REQUEST_CODE, viewLifecycleOwner) { _, data ->
-            val number = data.getInt(BoxFragment.EXTRA_RANDOM_NUMBER)
-            Toast.makeText(requireContext(), "Generated number: $number", Toast.LENGTH_SHORT).show()
+
+        val liveData =  findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<Int>(BoxFragment.EXTRA_RANDOM_NUMBER)
+        liveData?.observe(viewLifecycleOwner){number ->
+            if (number != null) {
+                Toast.makeText(requireContext(), "Generated number: $number", Toast.LENGTH_SHORT)
+                    .show()
+                liveData.value = null
+            }
         }
+
+//        parentFragmentManager.setFragmentResultListener(BoxFragment.REQUEST_CODE, viewLifecycleOwner) { _, data ->
+//            val number = data.getInt(BoxFragment.EXTRA_RANDOM_NUMBER)
+//            Toast.makeText(requireContext(), "Generated number: $number", Toast.LENGTH_SHORT).show()
+//        }
     }
 
     private fun openBox(color:Int, colorName : String){
+
+        val direction = RootFragmentDirections.actionRootFragmentToBoxFragment(
+            colorName,
+            color,
+        )
+
         findNavController().navigate(
-            R.id.action_rootFragment_to_boxFragment,
-            bundleOf(BoxFragment.ARG_COLOR to color, BoxFragment.ARG_COLOR_NAME to colorName )
+            direction,
+            navOptions {
+                anim {
+                    enter = R.anim.enter
+                    exit = R.anim.exit
+                    popEnter = R.anim.pop_enter
+                    popExit = R.anim.pop_exit
+                }
+            }
         )
     }
 }
